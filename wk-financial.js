9f1815136b857af2fcd04bc287e2446fed5040a9
William Kimball <wkkimball043@gmail.com>
2018-06-06 15:44:52 -0500

    added css

--- wk-financial.js
+++ wk-financial.js
@@ -1,4 +1,3 @@
-
 //function that creates the financial page
 let financialPageFunc = () => {
     const financialPage = {
@@ -46,73 +45,3 @@ let financialPageFunc = () => {
         article.appendChild(fragment);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //function for making a paragraph element
-// const mkPara = () => {
-//     return document.createElement('p');
-// }
-// //function for making a section element
-// const mkSec = () => {
-//     return document.createElement("section")
-// }
-// //function for making an h1 element
-// const mkH1 = () => {
-//     return document.createElement("h1")
-// }
-
-
-
-// //default function that creates a section for each object in the database
-// const defaultSectionMaker = () => {
-//     for (let obj in financialPage) {
-//         let array = financialPage[obj];
-
-//         for (i = 0; i < 1; i++) {
-//             let sec = mkSec();
-//             let h1 = mkH1();
-//             let para1 = mkPara();
-//             let para2 = mkPara();
-//             let para3 = mkPara();
-//             let para4 = mkPara();
-
-//             para1.textContent = `Bank Accounts: ${array[0]}`
-//             para2.textContent = `Businesses: ${array[1]}`;
-//             para3.textContent = `People Blackmailed: ${array[2]}`;
-//             para4.textContent = `Money laundering locations: ${array[3]}`;
-
-//             sec.appendChild(para1);
-//             sec.appendChild(para2);
-//             sec.appendChild(para3);
-//             sec.appendChild(para4);
-
-//             fragment.appendChild(sec)
-//         }
-
-//     } let article = document.querySelector('#art');
-//         article.appendChild(fragment)
-// } 
-
-
-//default on page load is to print everything
-// defaultSectionMaker();
