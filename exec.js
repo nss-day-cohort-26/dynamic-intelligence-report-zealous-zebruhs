c18acb2d1b97dcdab199d1ecf6b3e24b5b862e22
Jewel Ramirez <chimpy4u@yahoo.com>
2018-06-06 14:49:34 -0700

    adding files

--- exec.js
+++ exec.js
@@ -5,36 +5,57 @@ const execSummary = {
         alt: "Walter Off-White",
     },
     associates: {
-        pTag: ["Jesse Fushia-man", "Sal Bad-Dude"], 
-      sectionTitle: "Associates",
-      src: "https://vignette.wikia.nocookie.net/walkingdead/images/5/55/Jesse_Pinkman.jpg/revision/latest?cb=20150313041401",
-      alt: "associates",
+        pTag: ["Jesse Fushia-man ", "Sal Bad-Dude"],
+        sectionTitle: "Associates",
+        src: "https://vignette.wikia.nocookie.net/walkingdead/images/5/55/Jesse_Pinkman.jpg/revision/latest?cb=20150313041401",
+        alt: "associates",
     },
-      aliases: {
-          sectionTitle: "Aliases",
-          pTag: ["Walter Eggshell", "Walter beige"],     
-      }, 
-      country: {
-          sectionTitle: "Country",
-          pTag: ["United States of 'Merica"]
-      }
+    aliases: {
+        sectionTitle: "Aliases",
+        pTag: ["Walter Eggshell ", "Walter beige"],
+    },
+    country: {
+        sectionTitle: "Country",
+        pTag: ["United States of 'Merica"]
+    }
 
 }
 const summaryFragment = document.createDocumentFragment();
 
-
+function loadSummary() {
 for (key in execSummary) {
- if (execSummary[key].pTag != null) {
-     for (let i =0; i <= execSummary[key].pTag.length; i++) {
-let para = document.createElement("p");
-para.textContent = execSummary[key].pTag[i];
-console.log("stuff");
-summaryFragment.appendChild(para);
-     }
- }
-let sec = document.createElement("section");
-let hOne = document.createElement("h1");
 
+
+    if (execSummary[key].sectionTitle != null) {
+
+        let hOne = document.createElement("h1");
+        hOne.textContent = execSummary[key].sectionTitle;
+        summaryFragment.appendChild(hOne);
+    }
+
+    if (execSummary[key].src != null) {
+
+        let image = document.createElement("img");
+        image.src = execSummary[key].src
+        console.log(image)
+        summaryFragment.appendChild(image);
+    }
+    if (execSummary[key].pTag != null) {
+        let para = document.createElement("p");
+        para.textContent = execSummary[key].pTag;
+        console.log("stuff");
+        summaryFragment.appendChild(para);
+    }
 
 }
+}
+
+
+
+
+
+
+
+
+
 document.querySelector("#art").appendChild(summaryFragment)
