dcb0407f72752204322a615525b2620a81dcbabf
Austin Gorman <austin.gorman@gmail.com>
2018-06-06 15:21:36 -0500

    added clearing functionality

--- travel.js
+++ travel.js
@@ -6,9 +6,13 @@ const travelObject = {
   Rental: ["Albuquerque Meth Trailers", "Rented a movie from Redbox"]
 }
 
-const travelFragment = document.createDocumentFragment()
 
 const travelListBuilder = () => {
+  document.querySelector("article").innerHTML=""
+  const travelFragment = document.createDocumentFragment()
+  const h1Element = document.createElement("h1")
+  h1Element.textContent = "Travel Report"
+  travelFragment.appendChild(h1Element)
 
   for(let key in travelObject) {
     const sectionElement = document.createElement("section")
@@ -27,6 +31,3 @@ const travelListBuilder = () => {
     }
   }
 }
-
-
-travelListBuilder()
