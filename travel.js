70db14f86e307d443c28de6b584c7684536a90e6
Austin Gorman <austin.gorman@gmail.com>
2018-06-06 14:17:33 -0500

    Finished object structure for travel page

--- travel.js
+++ travel.js
@@ -3,26 +3,30 @@ const travelObject = {
   Airport: ["Albuquerque", "Mexico Airport Bueno", "Somewhere in the middle of nowhere that also has an airport for some reason"],
   Bus: ["Albuquerque", "Schmalbuquerque"],
   Cities: ["Albuquerque", "The Desert", "Albaquerque 2: Meth Methaloo"],
-  Rental: ["Albuquerque Meth Trailers", "Rented a movie from redbox"]
+  Rental: ["Albuquerque Meth Trailers", "Rented a movie from Redbox"]
 }
-
 
 const travelFragment = document.createDocumentFragment()
 
 const travelListBuilder = () => {
 
   for(let key in travelObject) {
-    let array = travelObject[key]
     const sectionElement = document.createElement("section")
+    const h2Element = document.createElement("h2")
     const ulElement = document.createElement("ul")
-    for(let i = 0; i < key[i].length; i++) {
-      const liElement = document.createElement("li")
-        liElement.textContent = `${key}: ${key[i]}`
-        ulElement.appendChild(liElement)
-        travelFragment.appendChild(ulElement)
-        console.log(travelFragment)
+    h2Element.textContent = `${key}: `
+    sectionElement.appendChild(h2Element)
+    sectionElement.appendChild(ulElement)
+    for(let i = 0; i < travelObject[key].length; i++) {
+      let liElement = document.createElement("li")
+      liElement.textContent = `${travelObject[key][i]}`
+      ulElement.appendChild(liElement)
+      travelFragment.appendChild(sectionElement)
+      let article = document.querySelector("#art")
+      article.appendChild(travelFragment)
     }
   }
 }
+
 
 travelListBuilder()
