8fec9b2aebf59fa9b0d7fccf85eb68cde7ba3a53
William Kimball <wkkimball043@gmail.com>
2018-06-07 15:46:16 -0500

    finished styling

--- exec.js
+++ exec.js
@@ -7,26 +7,27 @@ const execSummary = {
     },
     aliases: {
         sectionTitle: "Aliases",
-        pTag: ["Walter Eggshell ", "Walter beige"],
+        pTag: ["Walter Eggshell", " Walter beige"],
     },
     country: {
         sectionTitle: "Country",
         pTag: ["United States of 'Merica"]
     },
-    associates1: 
-          {
-        pTag: "Jesse Fushia-man",
-        sectionTitle: "Associates",
-        src: "https://vignette.wikia.nocookie.net/walkingdead/images/5/55/Jesse_Pinkman.jpg/revision/latest?cb=20150313041401",
-        alt: "associates"
-          },
-          associates2:
-          {
-        pTag: "Sal Bad-Man",
-        src: "https://vignette.wikia.nocookie.net/breakingbad/images/1/16/Saul_Goodman.jpg/revision/latest?cb=20120704065846",
-        alt: "associates"
-    } 
-    
+    associates: { sectionTitle: "Associates"},
+    associates1:
+        {
+            pTag: "Jesse Fushia-man",
+
+            src: "https://vignette.wikia.nocookie.net/walkingdead/images/5/55/Jesse_Pinkman.jpg/revision/latest?cb=20150313041401",
+            alt: "associates"
+        },
+    associates2:
+        {
+            pTag: "Sal Bad-Man",
+            src: "https://vignette.wikia.nocookie.net/breakingbad/images/1/16/Saul_Goodman.jpg/revision/latest?cb=20120704065846",
+            alt: "associates"
+        }
+
 
 }
 
@@ -37,29 +38,33 @@ function loadSummary() {
     document.querySelector("#art").innerHTML = ""
     // goes through the smaller objects in my big object
     for (key in execSummary) {
-
+        const detailFragment = document.createElement("div")
         // if there is a sectionTitle thingy it makes an h1
         if (execSummary[key].sectionTitle != null) {
 
             let hOne = document.createElement("h1");
             hOne.textContent = execSummary[key].sectionTitle;
-            summaryFragment.appendChild(hOne);
+            detailFragment.appendChild(hOne);
         }
         // if there is a src thingy it makes an image tag
         if (execSummary[key].src != null) {
-            console.log(execSummary[key]);
             let image = document.createElement("img");
             image.src = execSummary[key].src;
-            summaryFragment.appendChild(image);
-            
+            detailFragment.appendChild(image);
+
         }
         // if there is a pTag thingy it makes a p tag
         if (execSummary[key].pTag != null) {
             let para = document.createElement("p");
             para.textContent = execSummary[key].pTag;
-            summaryFragment.appendChild(para);
+            detailFragment.appendChild(para);
         }
+        if (key === "associates1" || key === "associates2") {
+            console.log("good")
+            detailFragment.className = "associateClass";
 
+        }
+        summaryFragment.appendChild(detailFragment)
     }
     // puts all my shit in the article
     document.querySelector("#art").appendChild(summaryFragment)
